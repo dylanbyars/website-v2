3401e8db354a74cd8e389a263531a52dc0af0bb9
Dylan <dylan@dylanbyars.com>
2018-10-05 14:44:48 -0400

    let's gooooo

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Portfolio for Dylan Byars',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Taken from "../images/taken.svg";
+
+const NotFoundPage = () => (
+  <div>
+    <h2 className="bg-yellow inline-block my-8 p-3">
+      Looks like this page is a ghost that got abducted by aliens...
+    </h2>
+  </div>
+);
+
+export default NotFoundPage;
